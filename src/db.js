3991906ae17d659712bb4a8fdec806442b638791
Hoàng Việt <hddt_hoangnmv>
2025-09-18 10:13:58 +0700

    update prj

--- src/db.js
+++ src/db.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const { Sequelize } = require('sequelize');
 
-const databaseFilePath = process.env.DATABASE_FILE || path.join(__dirname, '..', 'data.sqlite');
+const databaseUrl = process.env.DATABASE_URL;
+let sequelize;
 
-const sequelize = new Sequelize({
-	dialect: 'sqlite',
-	storage: databaseFilePath,
-	logging: false,
-});
+if (databaseUrl) {
+	sequelize = new Sequelize(databaseUrl, {
+		dialect: 'postgres',
+		protocol: 'postgres',
+		logging: false,
+		dialectOptions: process.env.NODE_ENV === 'production' ? { ssl: { require: true, rejectUnauthorized: false } } : {},
+	});
+} else {
+	const databaseFilePath = process.env.DATABASE_FILE || path.join(__dirname, '..', 'data.sqlite');
+	sequelize = new Sequelize({
+		dialect: 'sqlite',
+		storage: databaseFilePath,
+		logging: false,
+	});
+}
 
 module.exports = { sequelize };
 
